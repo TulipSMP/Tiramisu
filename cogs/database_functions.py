faf88be2eec2cd47757c070c9277cda20c2ab786
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-01-30 14:46:46 -0600

    streamline lib, ready for testing

--- cogs/database_functions.py
+++ cogs/database_functions.py
@@ -1,65 +1,66 @@
+# WARNING: This is NOT a cog and SHOULD NOT be loaded by bot.py!
 from logging42 import logger
-import nextcord
-from nextcord.ext import commands
 import yaml
 import mysql.connector
 import sqlite3
 
-class Database(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class Database():
+    def __init__(self):
+        logger.debug('Imported database_functions!')
 
     # Load Yaml
     # Instance Config
     with open("config/config.yml", "r") as ymlfile:
         cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     # What settings each guild should be able to load
-    with open("config/settings.yml", "r") as ymlfile:
-        settings = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    #with open("config/settings.yml", "r") as ymlfile:
+    #    settings = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
     # Database
-    logger.debug("Logging into DB from database.py")
-    if cfg["storage"] == "sqlite":
-        sql = sqlite3.connect('storage.db')
-        cursor = sql.cursor()
-    else:
-        sql = mysql.connector.connect(
-            host=cfg["mysql"]["host"],
-            user=cfg["mysql"]["user"],
-            password=cfg["mysql"]["pass"],
-            database=cfg["mysql"]["db"]
-        )
-        cursor = sql.cursor()
+    #logger.debug("Logging into DB from database.py")
+    #if cfg["storage"] == "sqlite":
+    #    sql = sqlite3.connect('storage.db')
+    #    cursor = sql.cursor()
+    #else:
+    #    sql = mysql.connector.connect(
+    #        host=cfg["mysql"]["host"],
+    #        user=cfg["mysql"]["user"],
+    #        password=cfg["mysql"]["pass"],
+    #        database=cfg["mysql"]["db"]
+    #    )
+    #    cursor = sql.cursor()
 
     # Functions
     # Creates tables for DB
     @logger.catch
-    def guild_tables_create(self, guild, table=None):
+    def guild_tables_create(cursor, guild, table=None):
         if table == 'admins' or table == None:
-            self.cursor(f'CREATE TABLE IF NOT EXISTS admins_{guild.id} ( id int, admin bit );')
+            cursor(f'CREATE TABLE IF NOT EXISTS admins_{guild.id} ( id int, admin bit );')
             logger.info(f'Created table "admins_{guild.id}", if it doesnt already exist!')
         if table == 'settings' or table == None:
-            self.cursor(f'CREATE TABLE IF NOT EXISTS settings_{guild.id} ( setting string, value string );')
+            with open("config/settings.yml", "r") as ymlfile:
+                settings = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            cursor(f'CREATE TABLE IF NOT EXISTS settings_{guild.id} ( setting string, value string );')
             for setting in settings['settings']:
-                self.cursor(f'INSERT INTO settings_{guild.id} ( setting, enabled ) VALUES ( {setting}, none );')
+                cursor(f'INSERT INTO settings_{guild.id} ( setting, enabled ) VALUES ( {setting}, none );')
             logger.info(f'Created table "settings_{guild.id}", if it doesnt already exist!')
     # Fetch information from DB
     # Default to settings if no table is specified
     @logger.catch
-    def guild_tables_fetch(self, guild, setting):
+    def guild_tables_fetch(cursor, guild, setting):
         if setting == None or setting == 'admin':
-            self.cursor(f'SELECT id FROM admins_{guild.id} WHERE admin=1;')
-            return self.cursor.fetchall()
+            cursor(f'SELECT id FROM admins_{guild.id} WHERE admin=1;')
+            return cursor.fetchall()
         else:
-            self.cursor(f'SELECT enabled FROM admins_{guild.id} WHERE admin=1;')
-            return self.cursor.fetchall()
+            cursor(f'SELECT enabled FROM admins_{guild.id} WHERE admin=1;')
+            return cursor.fetchall()
     # Change information in DB
     # Should ALWAYS return true if successfull, false if an error occurred
     @logger.catch
-    def guild_tables_set(self, guild, setting, value, clear=False):
+    def guild_tables_set(cursor, guild, setting, value, clear=False):
         if setting == 'admin' and clear == True:
             try:
-                self.cursor(f'DELETE FROM admins_{guild.id} WHERE id IS {value}')
+                cursor(f'DELETE FROM admins_{guild.id} WHERE id IS {value}')
                 logger.debug(f'Removed id {value} from table admins_{guild.id}')
                 return True
             except:
@@ -67,13 +68,12 @@ class Database(commands.Cog):
                 return False
         if setting == 'admin' and clear == False:
             try:
-                self.cursor(f'INSERT INTO admins_{guild.id} ( id, admin ) VALUES ( {value}, 1 )')
+                cursor(f'INSERT INTO admins_{guild.id} ( id, admin ) VALUES ( {value}, 1 )')
                 logger.debug(f'Added id {value} to table admins_{guild.id}')
                 return True
             except:
                 logger.debug(f'Failed to add id {value} to table admins_{guild.id}!')
                 return False
         
-def setup(bot):
-    bot.add_cog(Database(bot))
+def setup():
     logger.debug('Setup library "database"')
