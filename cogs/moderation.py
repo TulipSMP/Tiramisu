839ca5c113a6f51f2295fe0b51e5428112d3d76d
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-22 20:09:04 -0500

    load cfg outside of init for TESTING_GUILD_ID

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,7 +12,9 @@ class Moderation(commands.Cog):
         with open("config/config.yml", "r") as ymlfile:
             self.cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     
-    TESTING_GUILD_ID=self.cfg["discord"]["testing_guild"]
+    with open("config/config.yml", "r") as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    TESTING_GUILD_ID = cfg["discord"]["testing_guild"]
 
     # Error Function
     def error(self, error):
