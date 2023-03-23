85658786bc8c5eb457e904a46765d1c3c2dd0165
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-03-23 11:20:39 -0500

    `/ip` command

---
+++ cogs/utilities.py
@@ -0,0 +1,39 @@
+from logging42 import logger
+import nextcord
+from nextcord.ext import commands
+import yaml
+from libs.database import Database
+
+class Utilities(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    with open("config/config.yml", "r") as ymlfile:
+        self.cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    TESTING_GUILD_ID = self.cfg['discord']['testing_guild']
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info('Loaded cog catboy.py')
+
+    # Commands
+    @nextcord.slash_command(description="Get the Minecraft server IP", guild_ids=[TESTING_GUILD_ID])
+    async def ip(self, interaction: nextcord.Interaction):
+        db = Database(interaction.guild, reason='Slash command `/ip`')
+        ip = db.fetch('ip_address')
+        text = db.fetch('ip_text')
+        game = db.fetch('ip_game')
+        warn = ''
+        if ip == 'none':
+            warn += '\nAsk the admins to change the setting `ip_address`'
+        if text == 'none':
+            text = ''
+            warn += '\nAsk the admins to change the setting `ip_text` to show a description.'
+        if game == 'none':
+            warn += '\nAsk the admins to change the setting `ip_game` to which game their server is for.'
+        await interaction.send(f'**{game.title()} Server IP:** `{ip}`\n{text}{warn}')
+
+def setup(bot):
+    bot.add_cog(Catboy(bot))
+    logger.debug('Setup cog "catboy"')
