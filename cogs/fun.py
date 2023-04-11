beb590e639e1657fdc738578cfb3b8aeae0aea08
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-04-11 18:57:00 -0500

    Question on eight ball, and other QoL improvements

--- cogs/fun.py
+++ cogs/fun.py
@@ -2,6 +2,7 @@ from logging42 import logger
 import random
 import nextcord
 from nextcord.ext import commands
+from typing import Optional
 
 class FunCommands(commands.Cog):
     def __init__(self, bot):
@@ -14,7 +15,8 @@ class FunCommands(commands.Cog):
     
     # 8ball Command
     @nextcord.slash_command(description="Ask the magic 8ball a question!")
-    async def eight_ball(self, interaction: nextcord.Interaction):
+    async def eight_ball(self, interaction: nextcord.Interaction,
+    question: Optional[str] = nextcord.SlashOption(required=False, description="Ask the magic 8ball a question!", min_length=5, max_length=100)):
         responses = [
             "It is certain.",
             "It is decidedly so.",
@@ -40,19 +42,21 @@ class FunCommands(commands.Cog):
         response = random.choice(responses)
         if random.randint(1, 250) == 69:
             response = "Certainly. Fizzdev is a catboy."
-        await interaction.response.send_message(response)
+        await interaction.response.send_message(response + " 🎱")
     
     # Dice Command
     @nextcord.slash_command(description="Roll a dice!")
-    async def dice(self, interaction: nextcord.Interaction):
-        result = random.randint(1, 6)
-        await interaction.response.send_message(f"You rolled a {result}!")
+    async def dice(self, interaction: nextcord.Interaction, 
+    type: Optional[int] = nextcord.SlashOption(choices = {'d4':4, 'd6':6, 'd8':8, 'd10':10, 'd12':12, 'd20':20, 'd100':100}, 
+    description='Kind of dice to roll', required=False, default=6)):
+        result = random.randint(1, type)
+        await interaction.response.send_message(f"You rolled a {result}! 🎲")
     
     # Coinflip Command
     @nextcord.slash_command(description="Flip a coin!")
     async def coinflip(self, interaction: nextcord.Interaction):
         result = random.choice(["heads", "tails"])
-        await interaction.response.send_message(f"The coin landed on {result}!")
+        await interaction.response.send_message(f"The coin landed on {result}! 🪙")
     
 def setup(bot):
     bot.add_cog(FunCommands(bot))
