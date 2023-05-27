cbe7581f548b8494984623b0ca25a784aaa5c71c
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-05-27 10:32:28 -0500

    replace some functions with libs/ counterparts

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -2,7 +2,10 @@ from logging42 import logger
 import nextcord
 from nextcord.ext import commands
 import yaml
+
 from libs.database import Database
+from libs import utility, moderation
+
 from typing import Optional
 
 class Moderation(commands.Cog):
@@ -12,10 +15,6 @@ class Moderation(commands.Cog):
         with open("config/config.yml", "r") as ymlfile:
             self.cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
     
-    # Error Function
-    def error(self, error):
-        logger.error(f"Error in moderation.py: {error}")
-        return self.cfg['messages']['error'].replace('[[error]]', str(error))
 
     # No Permission Function
     def noperm(self, cmd, interaction):
@@ -68,45 +67,19 @@ class Moderation(commands.Cog):
             except Exception as e:
                 await interaction.send(self.error(e), ephemeral=True)
         else:
-            await interaction.send(self.noperm('warn', interaction), ephemeral=True)
+            await interaction.send(self.cfg['messages']['noperm'], ephemeral=True)
     
     @nextcord.slash_command(description="Kick a user from the server")
     async def kick(self, interaction: nextcord.Interaction, user: Optional[nextcord.Member] = nextcord.SlashOption(description='Who to kick', required=True), 
         reason: Optional[str] = nextcord.SlashOption(description='Why this user is being kicked.', default='No reason given.', required=False),
         dm: Optional[bool] = nextcord.SlashOption(description='Should the user be DMed about why they were kicked?', choices={'Yes':True, 'No':False}, required=False, default=True)):
-        """ Kick a User from the server """
+        """ Kick a User from the guild """
+        
         db = Database(interaction.guild, reason=f'Check for permission, `/kick`')
         if interaction.user.id in db.fetch('admins') or self.is_mod(interaction.user, db):
-            try:
-                if user.id == self.bot.user.id:
-                    await interaction.send('I cannot kick myself! If you want me to leave, have an admin kick me.')
-                    return
-                logger.debug(f'{interaction.user.id} kicked {user.id} for {reason}')
-                try:
-                    if dm:
-                        await user.send(f"*You have been kicked from __{interaction.guild.name}__! For:*\n>>> **{reason}**")
-                except nextcord.errors.HTTPException:
-                    await interaction.send(f'I cannot DM this user! Use the `dm` option if you do not want me to tell them why they were kicked.', ephemeral=True)
-                    return
-                try:
-                    interaction.guild.kick(user)
-                except:
-                    await interaction.send(f'Could not kick {user.name}!', ephemeral=True)
-                    return
-                try:
-                    modlog_channel = interaction.guild.get_channel( int(db.fetch('modlog_channel')) )
-                    if modlog_channel != None:
-                        await modlog_channel.send(f'{user.mention} ||{user.name}#{user.discriminator} ID: `{user.id}`|| was kicked by {interaction.user.name}#{interaction.user.discriminator} ID: `{interaction.user.id}`\nFor: {reason}')
-                        logging_info = f'This action was logged successfully in {modlog_channel.mention}.'
-                    else:
-                        logging_info = f'This action was not logged. Make sure the `modlog_channel` setting is correct.'
-                except:
-                    logging_info = f'This action was not logged. Make sure the `modlog_channel` setting is correct.'
-                await interaction.send(f'{user.mention} was successfully kicked from the server!\n*{logging_info}*', ephemeral=True)
-            except Exception as e:
-                await interaction.send(self.error(e), ephemeral=True)
+            moderation.kick(interaction, user, reason, dm=dm)
         else:
-            await interaction.send(self.noperm('kick', interaction), ephemeral=True)
+            await interaction.send(self.cfg['messages']['noperm'], ephemeral=True)
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
