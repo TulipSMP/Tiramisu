bc9166fb886596dd3bac71880306f20f506c5bb9
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-05-29 20:50:45 -0500

    remove unnecessary legacy code, and revert cogs actions to subcommands

--- bot.py
+++ bot.py
@@ -8,6 +8,8 @@ import sqlite3
 import yaml
 import shutil
 
+from libs import utility
+
 # Ensure Config exists:
 if os.path.exists('config/config.yml'):
     logger.info('Successfully found config/config.yml!')
@@ -25,37 +27,6 @@ with open("config/config.yml", "r") as ymlfile:
     logger.info(f'Successfully loaded config/config.yml: {cfg}')
 
 TESTING_GUILD_ID=cfg["discord"]["testing_guild"]
-
-# Database
-if cfg["storage"] == "sqlite":
-    sql = sqlite3.connect('storage.db')
-    cursor = sql.cursor()
-    logger.info(f'Using sqlite storage: storage.db')
-else:
-    import mysql.connector
-    sql = mysql.connector.connect(
-        host=cfg["mysql"]["host"],
-        user=cfg["mysql"]["user"],
-        password=cfg["mysql"]["pass"],
-        database=cfg["mysql"]["db"]
-    )
-    cursor = sql.cursor()
-    logger.info(f'Using mySQL storage: {cfg["mysql"]["user"]}@{cfg["mysql"]["host"]}, database: {cfg["mysql"]["db"]}')
-
-# Load Adminsitrators from DB
-cursor = sql.cursor()
-cursor.execute(f"CREATE TABLE IF NOT EXISTS admins (id BIGINT, permission INT);")
-cursor.execute("SELECT id FROM admins;")
-admins_raw = cursor.fetchall()
-### And parse the convoluted output
-admins = []
-for admin_id in admins_raw:
-    admin_str = f'{admin_id}'
-    admin_new = admin_str.replace(',', '')
-    admin_new = admin_new.replace('(', '')
-    admin_new = admin_new.replace(')', '')
-    admins.append(admin_new)
-
 
 # Load things from cfg
 bot_token = cfg["discord"]["token"]
@@ -77,8 +48,12 @@ async def on_ready():
 # Load Commands
 
 # List Cogs
-@bot.slash_command(description='List cogs', guild_ids=[TESTING_GUILD_ID])
+@bot.slash_command(description='Cog actions', guild_ids=[TESTING_GUILD_ID])
 async def cogs(interaction: nextcord.Interaction):
+    pass # Setup for following subcommands
+
+@cogs.subcommand(description='List cogs')
+async def list(interaction: nextcord.Interaction):
     if interaction.user.id in cfg['discord']['co_owners'] or interaction.user.id == cfg['discord']['owner']:
         cogs_list = ''
         for filename in os.listdir('./cogs'):
@@ -88,10 +63,9 @@ async def cogs(interaction: nextcord.Interaction):
         logger.debug(f"Listed cogs for {interaction.user}")
     else:
         await interaction.send(noperm, ephemeral=True)
-        logger.debug(noperm_log)
 
 # Load Cogs
-@bot.slash_command(description="Load cogs", guild_ids=[TESTING_GUILD_ID])
+@cogs.subcommand(description="Load cogs")
 async def load(interaction: nextcord.Interaction, extension=None):
     if interaction.user.id in cfg['discord']['co_owners'] or interaction.user.id == cfg['discord']['owner']:
         try:
@@ -106,10 +80,9 @@ async def load(interaction: nextcord.Interaction, extension=None):
             await interaction.send(f'The cog `{extension}` was not found.')
     else:
         await interaction.send(noperm, ephemeral=True)
-        logger.debug(noperm_log)
 
 # Unload Cogs
-@bot.slash_command(description="Unload cogs", guild_ids=[TESTING_GUILD_ID])
+@cogs.subcommand(description="Unload cogs")
 async def unload(interaction: nextcord.Interaction, extension):
     if interaction.user.id in cfg['discord']['co_owners'] or interaction.user.id == cfg['discord']['owner']:
         try:
@@ -121,7 +94,6 @@ async def unload(interaction: nextcord.Interaction, extension):
             await interaction.send(f'The cog `{extension}` was not found.')
     else:
         await interaction.send(noperm, ephemeral=True)
-        logger.debug(noperm_log)
 
 # Reload Cogs
 @bot.slash_command(description="Reload cogs", guild_ids=[TESTING_GUILD_ID])
@@ -139,7 +111,6 @@ async def reload(interaction: nextcord.Interaction, extension=None):
             await interaction.send(f'The cog `{extension}` was not found!')
     else:
         await interaction.send(noperm, ephemeral=True)
-        logger.debug(noperm_log)
 
 # Stop the Bot
 @bot.slash_command(description='Stop the bot', guild_ids=[TESTING_GUILD_ID])
