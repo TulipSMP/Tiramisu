81d7686b73c9b57306b796dbcd9701c1125674a6
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-06 16:02:09 -0500

    user command sends 3 args?

--- cogs/mod_rightclick.py
+++ cogs/mod_rightclick.py
@@ -27,8 +27,8 @@ class ModRightclick(commands.Cog):
 
     # Commands
     @nextcord.user_command()
-    async def test(interaction: nextcord.Interaction, member: nextcord.Member):
-        await interaction.send(f'Hiya {member.mention}')
+    async def test(interaction: nextcord.Interaction, member: nextcord.Member, x):
+        await interaction.send(f'Hiya {member.mention}\n```\n{x}\n```')
 
 def setup(bot):
     bot.add_cog(ModRightclick(bot))
