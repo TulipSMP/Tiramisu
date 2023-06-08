57abb997107b46cbe295692d6138dcab5be47262
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-08 13:20:43 -0500

    nvm that didnt work reverts prev.commit

--- libs/buttons.py
+++ libs/buttons.py
@@ -12,9 +12,7 @@ class HelloButton(menus.ButtonMenu):
     def __init__(self):
         """ Says Hello! """
         super().__init__(disable_buttons_after=True)
-        intents = nextcord.Intents.default()
-        intents.members = True
-        super().bot = commands.Bot(intents=intents)
+
 
     async def send_initial_message(self, interaction, channel):
         return await channel.send(f'Press the button below!')
