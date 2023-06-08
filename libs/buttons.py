f6d48ca8fba945170259b728bbe533d70c9e61cf
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-08 13:16:58 -0500

    fix ButtonStyle

--- libs/buttons.py
+++ libs/buttons.py
@@ -16,7 +16,7 @@ class HelloButton(menus.ButtonMenu):
     async def send_initial_message(self, interaction, channel):
         return await channel.send(f'Press the button below!')
 
-    @nextcord.ui.button(label='Try Me!', emoji="❓", custom_id='tiramisu:hello', style=nextcord.ui.ButtonStyle.blurple)
+    @nextcord.ui.button(label='Try Me!', emoji="❓", custom_id='tiramisu:hello', style=nextcord.ButtonStyle.blurple)
     async def on_hello(self, button, interaction: nextcord.Interaction):
         await interaction.send(f'Hello {interaction.user.display_name}!')
 
@@ -29,7 +29,7 @@ class TicketsButton(menus.ButtonMenu):
         """ Send message containing buttons """
         return await channel.send(f'## Create a Ticket\nClick the button below to create a ticket so that others can chat with you.')
 
-    @nextcord.ui.button(label='New Ticket',emoji="✅", custom_id='tiramisu:create_ticket', style=nextcord.ui.ButtonStyle.success)
+    @nextcord.ui.button(label='New Ticket',emoji="✅", custom_id='tiramisu:create_ticket', style=nextcord.ButtonStyle.success)
     async def on_create(self, button, interaction: nextcord.Interaction):
         await ticketing.create(interaction)
 
@@ -48,6 +48,6 @@ class TicketCloseButton(menus.ButtonMenu):
     async def send_initial_message(self, interaction, channel):
         return await channel.send(self.message)
 
-    @nextcord.ui.button(label='Close', emoji="❌", custom_id=f'tiramisu:close_ticket', style=nextcord.ui.ButtonStyle.red)
+    @nextcord.ui.button(label='Close', emoji="❌", custom_id=f'tiramisu:close_ticket', style=nextcord.ButtonStyle.red)
     async def on_close(self, button, interaction: nextcord.Interaction):
         await ticketing.close(interaction, self.thread, self.user)
