ad010fabaa5698baa16bf9ce0d3c9e17e5fd2ade
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-08 12:49:17 -0500

    more ticketing work

--- libs/buttons.py
+++ libs/buttons.py
@@ -17,6 +17,25 @@ class TicketsButton(menus.ButtonMenu):
         """ Send message containing buttons """
         return await channel.send(f'## Create a Ticket\nClick the button below to create a ticket so that others can chat with you.')
 
-    @nextcord.ui.button(emoji="✅", custom_id='tiramisu:create_ticket', style=nextcord.ui.ButtonStyle.success)
+    @nextcord.ui.button(label='New Ticket',emoji="✅", custom_id='tiramisu:create_ticket', style=nextcord.ui.ButtonStyle.success)
     async def on_create(self, button, interaction: nextcord.Interaction):
         await ticketing.create(interaction)
+
+class TicketCloseButton(menus.ButtonMenu):
+    def __init__(self, thread: nextcord.Thread, user: nextcord.Member, message: str=None):
+        """ Button for Closing a Ticket """
+        super().__init__(disable_buttons_after=True)
+        self.thread = thread
+        self.user = user
+
+        if message == None:
+            self.message = f'**{thread.name}**\nPress the button below to close this ticket.'
+        else:
+            self.message = message
+
+    async def send_initial_message(self, interaction, channel):
+        return await channel.send(self.message)
+
+    @nextcord.ui.button(label='Close', emoji="❌", custom_id=f'tiramisu:close_ticket_{user.id}', style=nextcord.ui.ButtonStyle.red)
+    async def on_close(self, button, interaction: nextcord.Interaction):
+        await ticketing.close(interaction, self.thread, self.user)
--- libs/ticketing.py
+++ libs/ticketing.py
@@ -6,10 +6,10 @@
 import nextcord
 import random
 
-from libs import utility, modals, buttons
+from libs import utility, modals, buttons, moderation
 from libs.database import Database
 
-async def create(interaction: nextcord.Interaction, reason: str = None):
+async def create(interaction: nextcord.Interaction, reason: str = None, buttons: bool = False):
     """ Create a Ticket """
     if reason == None:
         await interaction.response.send_modal(modals.InputModal('Create a Ticket', 'Topic of ticket', create)) # Call create again with reason
@@ -27,5 +27,22 @@ async def create(interaction: nextcord.Interaction, reason: str = None):
     else:
         ticket_number = 0
     ticket_number += 1
-    await interaction.channel.create_thread(name=f'Ticket #{ticket_number}')
     db.set('ticket_int', str(ticket_number))
+
+    if buttons:
+        raise NotImplementedError
+    else:
+        thread = await interaction.channel.create_thread(name=f'Ticket #{ticket_number}', type = nextcord.ChannelType.private_thread, 
+            reason=f'Created Ticket # {ticket_number} for {interaction.user.name}.')
+
+async def close(interaction: nextcord.Interaction, thread: nextcord.Thread, creator: nextcord.Member):
+    """ Close a Ticket """
+    user = interaction.user
+    await interaction.send('Closing ticket...', ephemeral=True)
+
+    await thread.edit(name=f'{thread.name} [Closed]', archived=True, locked=True)
+
+    await creator.send_message(f'{thread.name} has been closed. You can view it here: {thread.mention}.')
+
+    await moderation.modlog(interaction.guild, '🎟️ Ticket Closed', user, creator, additional = {'Thread':thread.mention})
+
