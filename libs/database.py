6bf9a423d071ec1f1c1eab858a0743c16c3a82e0
Krafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-21 15:45:31 -0500

    Fix SQL injection Prevention (#37) (#38)
    * fix return in utility.valid_setting() with str
    
    * redo sql injection prevention
    
    * remove sql injection prevention
    
    * show error in log message
    
    * rework sql statement
    
    * new sql injection prevention
    
    * proper SQL injection prevention

--- libs/database.py
+++ libs/database.py
@@ -221,11 +221,11 @@ class Database:
                 logger.info(f'Removed {setting} {value} from custom table {table}_{self.guild.id}')
         else:
             try:
-                self.cursor.execute(f'UPDATE "settings_{self.guild.id}" SET value=%(value)s WHERE setting="{setting}"', {'value':value})
+                self.cursor.execute(f'UPDATE "settings_{self.guild.id}" SET value = ? WHERE setting = "{setting}"', (value,))
                 logger.info(f'Set {setting} to {value} for table settings_{self.guild.id}')
                 return True
-            except:
-                logger.warning(f'Failed to set value {setting} to {value} for table settings_{self.guild.id}!')
+            except Exception as exception:
+                logger.warning(f'Failed to set value {setting} to {value} for table settings_{self.guild.id}!: {exception}')
                 return False
     
     # Send raw commands to Database
