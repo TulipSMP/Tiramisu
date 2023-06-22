470a47c905c80ddb3e266f4d5dba5231113ac51c
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-22 16:08:59 -0500

    add VC join/leave

--- libs/logging.py
+++ libs/logging.py
@@ -19,9 +19,10 @@ class LoggingEvent:
         self.title = title
         self.user = user
         self.extra = extra
+        self.void = False # Used to check if an action should be logged
     
     def message(self):
-        msg = f"{self.title}\nBy: {self.user.name} `{self.user.id}`"
+        msg = f"{self.title}\nAuthor: {self.user.name} `{self.user.id}`"
         for key in self.extra:
             msg += f"\n{key}: {self.extra[key]}"
         return msg
@@ -51,6 +52,7 @@ class DeletedMessage(LoggingEvent):
 
 class EditedMessage(LoggingEvent):
     def __init__(self, old: nextcord.Message, new:nextcord.Message):
+        """ Event for message being edited """
         super().__init__(
             old.guild,
             "**✏️ Edited Message **",
@@ -76,6 +78,24 @@ class EditedMessage(LoggingEvent):
         
         return attachments
 
+class ChangeVoice(LoggingEvent):
+    def __init__(self, member, before, after):
+        """ Event for Voice state update """
+        if before.channel == None and after.channel != None: # User joined a VC
+            action = 'Joined'
+        elif before.channel != None and after.channel == None: # User left VC
+            action = 'Left'
+        else:
+            self.void = True
+        
+        super().__init__(
+                member.guild, f"**🎤 {action} VC**",
+                member, extra = {
+                    "VC":after.channel.mention
+                }
+            )
+
+
 
 async def log(event: LoggingEvent):
     db = Database(event.guild, reason='Logging, fetch `log_channel`')
@@ -85,5 +105,6 @@ async def log(event: LoggingEvent):
             raise ValueError
     except ValueError:
         return
-    
-    await channel.send(event.message())
+
+    if not event.void:
+        await channel.send(event.message())
