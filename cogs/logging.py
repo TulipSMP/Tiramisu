d162454c40adc84129afd8eec7e4a43ff4d5c5a6
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-22 15:18:44 -0500

    inital logging: deleted message

---
+++ cogs/logging.py
@@ -0,0 +1,34 @@
+# 
+# Tiramisu Discord Bot
+# --------------------
+# Listener for libs.logging
+# 
+from logging42 import logger
+import nextcord
+from nextcord.ext import commands
+import yaml
+from libs import logging
+
+
+class Logging(commands.Cog):
+    def __init__(self, bot):
+        """ Boilerplate Cog. Also funny fizzdev catboy hahaha """
+        self.bot = bot    
+        with open("config/config.yml", "r") as ymlfile:
+            self.cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info('Loaded cog logging.py')
+
+    @self.bot.event()
+    async def on_message_message_delete(self, message):
+        await logging.log(logging.DeletedMessage(message))
+
+        
+
+def setup(bot):
+    bot.add_cog(Logging(bot))
+    logger.debug('Setup cog "logging"')
---
+++ libs/logging.py
@@ -0,0 +1,61 @@
+# 
+# Tiramisu Discord Bot
+# --------------------
+# Logging for Moderators
+# 
+import nextcord
+from libs.database import Database
+
+class LoggingEvent:
+    def __init__(self, guild: nextcord.Guild, title: str, user: nextcord.User, extra: dict = {}):
+        """ Base class for logging events
+        Parameters:
+         - `guild`: nextcord.Guild, the guild this is happening in
+         - `title`: str, the heading for the log message
+         - `user`: nextcord.User, the doer of the action
+         - `extra`: optional dict, fields for the message """
+
+        self.guild = guild
+        self.title = title
+        self.user = user
+        self.extra = extra
+    
+    def message(self):
+        msg = f"{self.title}\nBy: {self.user.name}"
+        for key in self.extra:
+            msg += f"{key}: __{self.extra[key]}__"
+        return msg
+
+class DeletedMessage(LoggingEvent):
+    def __init__(self, message: nextcord.Message):
+        """ Event for a message being deleted """
+        attachments = ''
+        for attachment in message.attachments:
+            if attachments == '':
+                attachments += attachment.filename
+            else:
+                attachments += f', {attachment.filename}'
+        if attachments == '':
+            attachments = 'None'
+
+        super().__init__(
+            message.guild, 
+            "**✏️ Deleted Message **", 
+            message.author,
+            extra = {
+                "Content":message.content,
+                "Attatchments":attachments
+            }
+            )
+
+
+async def log(event: LoggingEvent):
+    db = Database('Logging, fetch `log_channel`')
+    try:
+        channel = event.guild.get_channel(int(db.fetch('log_channel')))
+        if channel == None:
+            raise ValueError
+    except ValueError:
+        return
+    
+    await channel.send(event.message())
