5e50c3c1186494f1afc2983c2197c71942fd35bc
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-06-22 11:30:33 -0500

    add check if thread is ticlet

--- libs/ticketing.py
+++ libs/ticketing.py
@@ -72,17 +72,42 @@ To add people to the ticket, simply **@mention** them.')
     await interaction.send(f'*Ticket Opened in {thread.mention}*')
         
 
-#async def close(interaction: nextcord.Interaction):
-#    """ Close a Ticket """
-#    db = Database(interaction.guild, reason='Ticketing, close ticket')
-#    user = interaction.user
-#    if interaction.channel.type != nextcord.ChannelType.private_thread or interaction.channel:
-#        await interaction.send(f'Run this command in the ticket you wish to close.', ephemeral=True)
-#        return
-#
-#    await thread.edit(name=f'{thread.name} [Closed]', archived=True, locked=True)
-#
-#    await creator.send_message(f'{thread.name} has been closed. You can view it here: {thread.mention}.')
-#
-#    await moderation.modlog(interaction.guild, '🎟️ Ticket Closed', user, user, additional = {'Thread':thread.mention})
+async def close(interaction: nextcord.Interaction):
+    """ Close a Ticket """
+    db = Database(interaction.guild, reason='Ticketing, close ticket')
+    user = interaction.user
+    if interaction.channel.type != nextcord.ChannelType.private_thread or interaction.channel:
+        await interaction.send(f'Run this command in the ticket you wish to close.', ephemeral=True)
+        return
 
+    await thread.edit(name=f'{thread.name} [Closed]', archived=True, locked=True)
+
+    await creator.send_message(f'{thread.name} has been closed. You can view it here: {thread.mention}.')
+
+    await moderation.modlog(interaction.guild, '🎟️ Ticket Closed', user, user, additional = {'Thread':thread.mention})
+
+async def is_ticket(thread: nextcord.Thread):
+    """ Check if a Thread is a ticket
+    Returns: bool """
+    if thread.type != nextcord.ChannelType.private_thread:
+        return False # All tickets are private threads
+    
+    db = Database(thread.guild, reason='Ticketing, checking if thread is ticket')
+
+    if not db.fetch('ticket_channel').isdigit():
+        return False # We won't have threads if they're not enabled
+    elif int(db.fetch('ticket_channel')) != thread.parent_id:
+        return False # All threads are children of ticket_channel
+
+    number = thread.name.strip("Thread #")
+    if number.isdigit(): # Check if name is 'Thread #0' etc.
+        if not db.fetch('ticket_int').isdigit():
+            return False
+        if not int(number) >= int(db.fetch('ticket_int')):
+            return False
+    else:
+        return False
+    
+    return True
+
+    
