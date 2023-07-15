300de90a6c6ddd5058623f49a3cc297c84216da3
Krafter <75903664+TheKrafter@users.noreply.github.com>
2023-07-15 13:57:05 -0500

    BugTest Fixes (#56)
    * Remove extra parenthesis in report player
    Fixes #55
    
    * Convert rightclick demos to gifs
    Fixes #54
    
    * Add Descripton in /report user for user
    Fixes #53

--- cogs/reporting.py
+++ cogs/reporting.py
@@ -29,7 +29,8 @@ class Reporting(commands.Cog):
         pass
 
     @report.subcommand(description='Report a discord user to the moderators')
-    async def user(self, interaction: nextcord.Interaction, user: nextcord.Member,
+    async def user(self, interaction: nextcord.Interaction,
+        user: Optional[nextcord.Member] = nextcord.SlashOption(description='Who to Report', required=True),
         reason: Optional[str] = nextcord.SlashOption(description='Why are you reporting this user?', required=True),):
         db = Database(interaction.guild, reason='Slash command `/report user`')
         try:
@@ -55,11 +56,12 @@ class Reporting(commands.Cog):
                 raise ValueError
             else:
                 await modlog_channel.send(f'**Minecraft Player Reported:**\nBy: {interaction.user.name}#{interaction.user.discriminator} (`{interaction.user.id}`)\
-\nReported User: `{player}`)\nReason: {reason}')
+\nReported User: `{player}`\nReason: {reason}')
                 logger.info('Successfully completed a report player action.')
                 await interaction.send(f'Successfully sent your report to the moderators! Thanks for speaking up.', ephemeral=True)
         except ValueError:
-            await interaction.send('The moderators have not yet (or incorrectly) set up where to send reports!\nAsk them to set the `modlog_channel` setting to the ID of the channel where logs should be sent.', ephemeral=True)
+            await interaction.send('Administrators have not yet (or incorrectly) set up where to send reports!\nAsk them to set the `modlog_channel` setting to the ID of the channel where logs should be sent.', ephemeral=True)
+
     @report.subcommand(description='Report a minecraft player to the moderators')
     async def bug(self, interaction: nextcord.Interaction, 
         place: Optional[str] = nextcord.SlashOption(description='Where does this bug occur?', required=True),
