2ccf24721aba87ab2cbf01c4185ba477b040b56b
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-09-01 21:56:52 -0500

    didnt mean to commit in-progress cog ☠️

--- cogs/actionlog.py
+++ cogs/actionlog.py
@@ -18,20 +18,20 @@ class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
-    with open("config/config.yml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-    TESTING_GUILD_ID=cfg["discord"]["testing_guild"]
+    #with open("config/config.yml", "r") as ymlfile:
+    #    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    #TESTING_GUILD_ID=cfg["discord"]["testing_guild"]
 
     # Events
-    @commands.Cog.listener()
-    async def on_ready(self):
-        logger.info('Loaded cog hello.py')
+    #@commands.Cog.listener()
+    #async def on_ready(self):
+    #    logger.info('Loaded cog hello.py')
 
     # Commands
-    @nextcord.slash_command(description="Hello!", guild_ids=[TESTING_GUILD_ID])
-    async def hello(self, interaction: nextcord.Interaction):
-        await interaction.response.send_message(f"Hello {interaction.user.display_name}! I'm {self.bot.user.display_name}!")
-        logger.debug(f"Said hello to {interaction.user.name}.")
+    #@nextcord.slash_command(description="Hello!", guild_ids=[TESTING_GUILD_ID])
+    #async def hello(self, interaction: nextcord.Interaction):
+    #    await interaction.response.send_message(f"Hello {interaction.user.display_name}! I'm {self.bot.user.display_name}!")
+    #    logger.debug(f"Said hello to {interaction.user.name}.")
 
 def setup(bot):
     bot.add_cog(Hello(bot))
