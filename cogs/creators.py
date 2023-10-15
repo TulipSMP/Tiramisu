e085fe1fb47b20b53a223768d179efe81cad6ee8
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-10-14 19:42:44 -0500

    feat: Content Creators * this is the initial rough draft * corresponds to #78

---
+++ cogs/creators.py
@@ -0,0 +1,66 @@
+# 
+# Tiramisu Discord Bot
+# --------------------
+# Content Creators
+# 
+from logging42 import logger
+import nextcord
+from nextcord.ext import commands
+import yaml
+from typing import Optional
+
+from libs.database import Database
+from libs import youtube
+
+class Creators(commands.Cog):
+    def __init__(self, bot):
+        """ Creators Cog """
+        self.bot = bot    
+        with open("config/config.yml", "r") as ymlfile:
+            self.cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+
+
+    # Events
+    @commands.Cog.listener()
+    async def on_ready(self):
+        logger.info('Loaded cog creators.py')
+
+    # Commands
+    @nextcord.slash_command(description="Commands for Content Creators")
+    async def creator(self, interaction: nextcord.Interaction):
+        pass # for subcommands
+
+    @creator.subcommand(description="Set channel ID")
+    async def channel(self, interaction: nextcord.Interaction, 
+        id: Optional[str] = nextcord.SlashOption(description="Channel ID from youtube.com/channel/CHANNELID", required=False)):
+        db = Database(interaction.guild, reason='Creators, check perms')
+        try:
+            creator_role = interaction.guild.get_role(int(db.fetch('creator_role')))
+            if creator_role == None:
+                raise ValueError
+        except ValueError:
+            await interaction.send('Content Creators are not yet set up.\n*Ask an administrator to set the `creator_role` and `creator_channel` settings (see `/help topic:Content Creators`)*')
+            return
+        
+        if creator_role in interaction.user.roles:
+            if id == None:
+                youtube.update_creator(interaction.guild, interaction.user, 'none')
+                await interaction.send('Removed channel from your account.')
+                return
+
+            feed = youtube.validate_yt(id)
+            if feed == None:
+                await interaction.send("Invalid Channel ID.\n*If you're signed into the account your channel is associated with, you can [copy the Channel ID here](<https://www.youtube.com/account_advanced>). If not, see [this support page](<https://support.google.com/youtube/answer/3250431>).*",
+                    ephemeral=True)
+                return
+            else:
+                youtube.update_creator(interaction.guild, interaction.user, feed)
+                data = youtube.get_feed_data(feed)
+                await interaction.send(f"Updated Channel to [{data['name']}](<{data['link']}>).")
+        else:
+            await interaction.send(self.cfg['messages']['noperm'])
+
+
+def setup(bot):
+    bot.add_cog(Creators(bot))
+    logger.debug('Setup cog "creators"')
---
+++ libs/youtube.py
@@ -0,0 +1,123 @@
+# 
+# Tiramisu Discord Bot
+# --------------------
+# Youtube Subscription
+# 
+from logging42 import logger
+
+import yaml
+import feedparser
+import nextcord
+import dateutil.parser
+import datetime
+import time
+
+from typing import Optional, Union, List
+from libs.database import Database
+
+def validate_yt(id: str) -> Optional[str]:
+    """ Convert YT channel ID into RSS feed URL. """
+    try:
+        url = f'https://www.youtube.com/feeds/videos.xml?channel_id={id}'
+        result = feedparser.parse(url)
+        if result['bozo'] != False:
+            return None
+        return url
+    except:
+        return None
+
+# Create + validate tables for a guild
+# user int | feed str | checked int
+# (discord user id) | (rss feel url) | (last checked timestamp)
+def setup_tables(guild: nextcord.Guild) -> None:
+    """ Setup Content Creator database tables for a guild """
+    db = Database(guild, reason='YouTube, setup table')
+    db.raw(f'CREATE TABLE IF NOT EXISTS "creators_{db.guild.id}" (user int, feed str, checked bigint);', fetch=False)
+    db.close()
+
+# Set/remove table row
+def update_creator(guild: nextcord.Guild, user: Union[nextcord.Member, nextcord.User], feed: str = 'none') -> None:
+    """ Add a creator to the table if it is not already there """
+    db = Database(guild, reason='YouTube, edit creator')
+    if user.id in db.raw(f'SELECT user FROM "creators_{db.guild.id}";'):
+        db.raw(f'UPDATE "creators_{db.guild.id}" SET feed=? WHERE user={user.id}', (feed), fetch=False)
+    else:
+        db.raw(f'INSERT INTO "creators_{db.guild.id}" (user, feed, checked) VALUES ( ? , ? , ? );', 
+            ( user.id, feed, int(time.time()) ), fetch=False)
+    db.close()
+
+def get_feed_data(url: str) -> Optional[dict]:
+    """ Simplifies feed data """
+    raw = feedparser.parse(url)
+    entries = []
+    for entry in raw['entries']:
+        entries.append(
+            {
+                'title': entry['title'],
+                'author': entry['author'],
+                'author_url': entry['author_detail']['href'],
+                'url': entry['link'],
+                'id': entry['yt_videoid'],
+                'published': dateutil.parser.parse(entry['published']).timestamp(),
+                'description': entry['summary'],
+                'thumbnail': entry['media_thumbnail'][0]['url'],
+            }
+        )
+
+    basic = {
+        'name': raw['title'],
+        'link': raw['link'],
+        'entries': entries
+    }
+    return basic
+
+# Check for new videos for a guild
+async def check_for_new(guild: nextcord.Guild, post: bool = True) -> Optional[List[dict]]:
+    """ Check for new videos and return a list of them (as formatted by get_feed_data() ) """
+    db = Database(guild, reason='YouTube, check for new posts')
+    try:
+        creator_channel = guild.get_channel(int(db.fetch('creator_channel')))
+        if creator_channel == None:
+            raise ValueError
+    except ValueError:
+        return # Channel is not set up.
+    creators = db.raw(f'SELECT (user, feed, checked) FROM "creators_{db.guild.id}";')
+
+    new_posts= [] 
+    for creator in creators:
+        try:
+            last_check = creator[2] # Timestamp of when last checked
+            db.raw(f'UPDATE "creators_{db.guild.id}" SET checked={time} WHERE user={creator.id};', fetch=False) # Update timestamp
+            content = get_feed_data(creator[1])
+            for entry in content['entries']:
+                if entry['published'] >= last_check:
+                    new_posts.append(entry)
+        except:
+            return # fail silently
+
+    if post:
+        try:
+            ping = guild.get_role(int(db.fetch('creator_ping_role')))
+        except ValueError:
+            ping = None
+        for item in new_posts:
+            await post_video(creator_channel, item, None, ping)
+    db.close()
+    return new_posts
+
+
+async def post_video(channel: nextcord.Channel, video: dict, creator: Optional[Union[nextcord.Member, nextcord.User]], 
+    ping: Optional[nextcord.Role]):
+    """ Announce video in channel """
+    if creator != None:
+        note = f' ({creator.mention})'
+    else:
+        note = ''
+    if ping != None:
+        mention = f'\n{ping.mention}'
+    else:
+        mention = ''
+    msg = f'## [{video['title']}]({video['url']})\nby [{video['author']}](<{video['author_url']}>){note}\n>>>{video['description'].split('\n')[0]}{mention}'
+
+    await channel.send(msg)
+
