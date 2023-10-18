4cf7d60941b2bad1597bb5bbecbf7317ab240fd0
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-10-18 17:50:31 -0500

    only disable on  "No"

--- libs/buttons.py
+++ libs/buttons.py
@@ -93,5 +93,5 @@ class TicketCloseConfirmation(nextcord.ui.View):
 
     @nextcord.ui.button(label='No', emoji='🛑', style=nextcord.ButtonStyle.red)
     async def on_no(self, button, interaction: nextcord.Interaction):
-        #await self.disable_self(interaction)
+        await self.disable_self(interaction)
         pass
