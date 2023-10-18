50fda5d63b74c88dc8dad4700b0726219ec53f74
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2023-10-18 17:55:44 -0500

    tidyup

--- libs/buttons.py
+++ libs/buttons.py
@@ -88,10 +88,8 @@ class TicketCloseConfirmation(nextcord.ui.View):
 
     @nextcord.ui.button(label='Yes', emoji='✅', style=nextcord.ButtonStyle.green)
     async def on_yes(self, button, interaction: nextcord.Interaction):
-        #await self.disable_self(interaction)
         await ticketing.close(interaction, confirmed=True)
 
     @nextcord.ui.button(label='No', emoji='🛑', style=nextcord.ButtonStyle.red)
     async def on_no(self, button, interaction: nextcord.Interaction):
         await self.disable_self(interaction)
-        pass
